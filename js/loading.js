bcab58ddaf1e0d65faa8386eb2e73b2f1a0feff1
xulidbj <2414457987@qq.com>
2018-11-02 18:04:45 +0800

    first commit

---
+++ js/loading.js
@@ -0,0 +1,13 @@
+//加載完點擊跳至主頁面
+/*
+$(window).load(function() {
+	//$("#loading").delay(2000).fadeOut(500);
+	$("#loading-center").click(function() {
+	$("#loading").fadeOut(500);
+	})
+})
+*/
+//加載完跳至主頁面
+ $(window).load(function() {
+   $("#loading").fadeOut(500);
+})
